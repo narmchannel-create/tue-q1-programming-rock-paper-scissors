bd16e989e2fac4d71c8719ab0ec9a7a3745ccfa3
moonsol124 <moonsol124@gmail.com>
2025-10-05 12:20:47 +0200

    initial setup

---
+++ Game.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * goal is to use
+ * 1. inheritance
+ * 2. polymorphism
+ * 3. abstract classes / interfaces / subclasses
+ * 4. objected oriented programming
+ * 5. division of methods
+ * 6. lisvoke substitution
+ * 
+ * idea:
+ * 1. game play option with abstract class thus inheritance: rock, paper, scissors
+ * 2. game rule checker with interface: rock checker, paper checker, scissor checker
+ * 3. decides who wins, by using the rule checkers, thus applying polymorphism and lisvoke substitution
+ */
+
+public class Game {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+    }    
+}
